0921dd8191e292ea3d340dc6eb8a4313a2643437
ahmedfatnassi <ahmedfatnassi23@gmail.com>
2019-08-08 00:33:40 +0100

    fix service and entities

--- notification/src/main/java/com/notification/domain/notification/notification/controller/notificationController.java
+++ notification/src/main/java/com/notification/domain/notification/notification/controller/notificationController.java
@@ -1,8 +1,24 @@
 package com.notification.domain.notification.notification.controller;
 
+import com.notification.domain.notification.notification.entities.Notification;
+import com.notification.domain.notification.notification.service.NotificationService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("notification")
 @RestController
 public class notificationController {
+    private final NotificationService notificationService ;
+     @GetMapping
+    public List<Notification> findAll(){
+         return this.notificationService.findAll();
+     }
+     @PostMapping()
+    public Notification create(@RequestBody Notification notification){
+         return this.notificationService.create(notification);
+     }
 }
